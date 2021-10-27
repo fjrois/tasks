d1bb81f2679b9b7cd675bf0aeff6f5856d0e0365
fjrois <fjroissiso@gmail.com>
2021-10-27 13:14:47 -0700

    Added firebase config

---
+++ src/config.js
@@ -0,0 +1,11 @@
+export default {
+  firebase: {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: 'tasks-f3816.firebaseapp.com',
+    projectId: 'tasks-f3816',
+    storageBucket: 'tasks-f3816.appspot.com',
+    messagingSenderId: '979516677467',
+    appId: '1:979516677467:web:fd282e8de7d88f98d18e7e',
+    measurementId: 'G-NE37LF7LZ6',
+  },
+};
