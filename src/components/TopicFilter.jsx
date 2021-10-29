f77c5d7651aaa4d2a7e15db68787ef228b41069c
fjrois <fjroissiso@gmail.com>
2021-10-29 09:57:31 -0700

    Created TopicFilter component

---
+++ src/components/TopicFilter.jsx
@@ -0,0 +1,47 @@
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+
+export default function TopicFilter({
+  selectedTopicId,
+  setSelectedTopicId,
+  topics,
+}) {
+  return (
+    <ToggleButtonGroup
+      size="small"
+      // orientation="vertical"
+      orientation="horizontal"
+      value={selectedTopicId}
+      exclusive
+      onChange={(event, nextView) => {
+        setSelectedTopicId(nextView);
+      }}
+    >
+      {Object.keys(topics).map((topicId) => (
+        <ToggleButton
+          key={topicId}
+          size="small"
+          sx={{ minWidth: '45px' }}
+          disableRipple
+          value={topicId}
+          aria-label="list"
+        >
+          {<div>{topics[topicId].name}</div>}
+          {/* {<div>{abbreviateName(topics[topicId].name)}</div>} */}
+        </ToggleButton>
+      ))}
+    </ToggleButtonGroup>
+  );
+}
+
+// TODO: move abbreviation to topic creation, and handle duplicates (T,Ta...)
+// function abbreviateName(name) {
+//   if (!name) return name;
+//   const words = name.split(' ').map((word) => word.trim());
+//   const abbreviation = words.reduce(
+//     (acc, item) => acc + item[0].toUpperCase(),
+//     ''
+//   );
+
+//   return abbreviation;
+// }
