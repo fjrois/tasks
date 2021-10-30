60410619161ca02644149129d777b71410260472
fjrois <fjroissiso@gmail.com>
2021-10-29 21:42:48 -0700

    Extended TopicFilter to support deletions and topics array

--- src/components/TopicFilter.jsx
+++ src/components/TopicFilter.jsx
@@ -2,8 +2,9 @@ import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 
 export default function TopicFilter({
+  deleteTopic,
   selectedTopicId,
-  setSelectedTopicId,
+  setSelectedTopicIndex,
   topics,
 }) {
   return (
@@ -13,27 +14,38 @@ export default function TopicFilter({
       orientation="horizontal"
       value={selectedTopicId}
       exclusive
-      onChange={(event, nextView) => {
-        setSelectedTopicId(nextView);
+      onChange={(event, newValue) => {
+        setSelectedTopicIndex(newValue);
       }}
     >
-      {Object.keys(topics).map((topicId) => (
-        <ToggleButton
-          key={topicId}
-          size="small"
-          sx={{
-            borderColor: 'rgb(196,196,196)',
-            color: 'rgb(102,102,102)',
-            minWidth: '45px',
-          }}
-          disableRipple
-          value={topicId}
-          aria-label="list"
-        >
-          {<div>{topics[topicId].name}</div>}
-          {/* {<div>{abbreviateName(topics[topicId].name)}</div>} */}
-        </ToggleButton>
-      ))}
+      {topics
+        ? topics.map((topic, index) => (
+            <ToggleButton
+              onMouseDown={(event) => {
+                // (Middle click) or (Right click + shift)
+                if (
+                  event.button === 1 ||
+                  (event.button === 0 && event.shiftKey)
+                ) {
+                  deleteTopic(topic);
+                }
+              }}
+              key={topic.id}
+              size="small"
+              sx={{
+                borderColor: 'rgb(196,196,196)',
+                color: 'rgb(102,102,102)',
+                minWidth: '45px',
+              }}
+              disableRipple
+              value={index}
+              aria-label="list"
+            >
+              {<div>{topic.name}</div>}
+              {/* {<div>{abbreviateName(topics[topicId].name)}</div>} */}
+            </ToggleButton>
+          ))
+        : null}
     </ToggleButtonGroup>
   );
 }
