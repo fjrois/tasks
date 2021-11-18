2fca493494067d3d58def20c9f183b92c22118cc
fjrois <fjroissiso@gmail.com>
2021-11-17 23:51:28 -0800

    Sorted all tasks view by date created

--- src/components/AllTasksView.jsx
+++ src/components/AllTasksView.jsx
@@ -3,46 +3,50 @@ import Box from '@mui/material/Box';
 import ListItem from './ListItem';
 
 export default function AllTasksView({ tasksList, moveTaskToSelectedPanel }) {
-  function getBackgroundColor(status) {
-    switch (status) {
-      case 'doing':
-        return 'warning.main';
-      case 'done':
-        return 'success.main';
-      default:
-        return 'warning.light';
-    }
-  }
+  const sortedTasksList = tasksList
+    ? [...tasksList].sort(
+        (task1, task2) => task2.dateCreated - task1.dateCreated
+      )
+    : [];
 
   return (
     <>
-      {tasksList ? (
-        <Box
-          display="flex"
-          flexWrap="wrap"
-          // justifyContent="space-between"
-          columnGap="16px"
-          rowGap="10px"
-        >
-          {tasksList.map((task) => {
-            return (
-              <Box key={task.id} width="200px">
-                <ListItem
-                  backgroundColor={getBackgroundColor(task.status)}
-                  handleOnClick={
-                    moveTaskToSelectedPanel
-                      ? () => {
-                          moveTaskToSelectedPanel(task);
-                        }
-                      : null
-                  }
-                  task={task}
-                />
-              </Box>
-            );
-          })}
-        </Box>
-      ) : null}
+      <Box
+        display="flex"
+        flexWrap="wrap"
+        // justifyContent="space-between"
+        columnGap="16px"
+        rowGap="10px"
+      >
+        {sortedTasksList.map((task) => {
+          return (
+            <Box key={task.id} width="200px">
+              <ListItem
+                backgroundColor={getBackgroundColor(task.status)}
+                handleOnClick={
+                  moveTaskToSelectedPanel
+                    ? () => {
+                        moveTaskToSelectedPanel(task);
+                      }
+                    : null
+                }
+                task={task}
+              />
+            </Box>
+          );
+        })}
+      </Box>
     </>
   );
+}
+
+function getBackgroundColor(status) {
+  switch (status) {
+    case 'doing':
+      return 'warning.main';
+    case 'done':
+      return 'success.main';
+    default:
+      return 'warning.light';
+  }
 }
