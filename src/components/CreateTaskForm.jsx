ce4c98f0e0c5dca2a1ba3ad43e6968ad3ceac7b5
fjrois <fjroissiso@gmail.com>
2021-11-18 23:34:42 -0800

    Moved task creation logic and state to new component CreateTaskForm

---
+++ src/components/CreateTaskForm.jsx
@@ -0,0 +1,118 @@
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import Autocomplete from '@mui/material/Autocomplete';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import ClearIcon from '@mui/icons-material/Clear';
+import IconButton from '@mui/material/IconButton';
+import TextField from '@mui/material/TextField';
+
+import InputTopicSelector from './InputTopicSelector.jsx';
+import useLocalStorageState from '../hooks/useLocalStorageState.js';
+
+export default function CreateTaskForm({
+  addTask,
+  allowInput,
+  selectedInputTopicId,
+  setSelectedInputTopicId,
+  topicsList,
+}) {
+  const [inputTaskTitle, setInputTaskTitle] = useLocalStorageState({
+    debounce: true,
+    defaultValue: '',
+    key: 'tasks:input-task-title',
+  });
+
+  const [everCreatedTaskTitles, setEverCreatedTaskTitles] =
+    useLocalStorageState({
+      debounce: true,
+      defaultValue: [],
+      key: 'everCreatedTaskTitles',
+    });
+
+  return (
+    <Box paddingTop="10px" paddingBottom="4px">
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          if (inputTaskTitle) {
+            const taskCreated = addTask({
+              taskTitle: inputTaskTitle,
+            });
+            if (taskCreated) {
+              if (!everCreatedTaskTitles.includes(inputTaskTitle)) {
+                setEverCreatedTaskTitles((everCreatedTaskTitles) => {
+                  const taskTitles = [...everCreatedTaskTitles, inputTaskTitle];
+                  while (taskTitles.length > 5) taskTitles.shift();
+                  return taskTitles;
+                });
+              }
+              setInputTaskTitle('');
+            }
+          }
+        }}
+      >
+        <Box display="flex" gap="10px">
+          <InputTopicSelector
+            selectedTopicId={selectedInputTopicId ? selectedInputTopicId : ''}
+            setSelectedTopicId={setSelectedInputTopicId}
+            topicsList={topicsList}
+          />
+          <Autocomplete
+            sx={{ width: '100%' }}
+            id="inputTaskTitle"
+            disabled={!allowInput}
+            value={inputTaskTitle || ''}
+            inputValue={inputTaskTitle || ''}
+            onChange={(event, newValue) => {
+              setInputTaskTitle(newValue);
+            }}
+            onInputChange={(event, newValue) => {
+              const updatedValue =
+                newValue.length === 1 ? newValue.toUpperCase() : newValue;
+              setInputTaskTitle(updatedValue);
+            }}
+            freeSolo
+            disableClearable
+            // options={everCreatedTaskTitles.map((option) => option)}
+            options={[].map((option) => option)}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Add Task"
+                size="small"
+                //     InputProps={{
+                //       ...params.InputProps,
+                //       type: 'search',
+                //     }}
+                InputProps={{
+                  endAdornment: inputTaskTitle ? (
+                    <IconButton
+                      size="small"
+                      onClick={() => {
+                        setInputTaskTitle('');
+                      }}
+                    >
+                      <ClearIcon fontSize="10" />
+                    </IconButton>
+                  ) : null,
+                }}
+              />
+            )}
+          />
+          <Button
+            sx={{
+              borderColor: 'rgb(196,196,196)',
+              width: '13.8%',
+              minWidth: '40px',
+            }}
+            size="small"
+            variant="outlined"
+            type="submit"
+          >
+            <ArrowForwardIcon color="action" fontSize="small" />
+          </Button>
+        </Box>
+      </form>
+    </Box>
+  );
+}
--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,20 +1,13 @@
-// import { isChrome, isChromium } from 'react-device-detect';
 import React, { useEffect } from 'react';
 import confetti from 'canvas-confetti';
 
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import Autocomplete from '@mui/material/Autocomplete';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import ClearIcon from '@mui/icons-material/Clear';
 import Grid from '@mui/material/Grid';
-import IconButton from '@mui/material/IconButton';
 import TextField from '@mui/material/TextField';
 
 import AllTasksView from './AllTasksView.jsx';
+import CreateTaskForm from './CreateTaskForm.jsx';
 import ItemStack from './ItemStack.jsx';
-import InputTopicSelector from './InputTopicSelector.jsx';
-import LinearProgressWithLabel from './progress/LinearProgressWithLabel';
 import TopicsFilter from './TopicFilter.jsx';
 import useLocalStorageState from '../hooks/useLocalStorageState.js';
 import ViewSelector from './ViewSelector.jsx';
@@ -87,12 +80,6 @@ export default function Panel({
   updateTask,
   userId,
 }) {
-  const [inputTaskTitle, setInputTaskTitle] = useLocalStorageState({
-    debounce: true,
-    defaultValue: '',
-    key: 'tasks:input-task-title',
-  });
-
   const [inputTopicName, setInputTopicName] = useLocalStorageState({
     debounce: true,
     defaultValue: '',
@@ -103,6 +90,7 @@ export default function Panel({
     defaultValue: 2,
     key: 'tasks:stacks-count',
   });
+
   const showDoingStack = stacksCount === 3;
 
   const [selectedTopicFilterIndex, setSelectedTopicFilterIndex] =
@@ -140,13 +128,6 @@ export default function Panel({
       ? filteredTasksList.filter((task) => task?.panelId === panelId)
       : filteredTasksList;
 
-  const [everCreatedTaskTitles, setEverCreatedTaskTitles] =
-    useLocalStorageState({
-      debounce: true,
-      defaultValue: [],
-      key: 'everCreatedTaskTitles',
-    });
-
   // const prevProgressRef = useRef(0);
   // const prevPotentialProgressRef = useRef(0);
   useEffect(() => {
@@ -177,168 +158,72 @@ export default function Panel({
 
   return (
     <>
-      {/* <Box
+      <CreateTaskForm
+        allowInput={allowInput}
+        addTask={({ taskTitle }) =>
+          addTask({
+            taskTitle,
+            topic: selectedInputTopic,
+            panelId,
+          })
+        }
+        selectedInputTopicId={selectedInputTopicId}
+        setSelectedInputTopicId={setSelectedInputTopicId}
+        topicsList={topicsList}
+      />
+      <Box
+        marginTop="10px"
         sx={{
           display: 'flex',
-          flexWrap: 'wrap',
-          '& > :not(style)': {
-            m: 1,
-            width: 128,
-            height: 128,
-          },
-        }}
-      > */}
-      <Box
-        sx={{
-          marginTop: '9px',
-          width: '99%',
-          marginLeft: 'auto',
-          marginRight: 'auto',
+          flexFlow: 'row wrap',
+          justifyContent: 'space-between',
+          gap: '10px',
         }}
       >
-        <LinearProgressWithLabel hidelabel={1} progress={panelProgress} />
-      </Box>
-      <Box paddingTop="10px" paddingBottom="4px">
-        <form
-          onSubmit={(event) => {
-            event.preventDefault();
-            if (inputTaskTitle) {
-              const taskCreated = addTask(
-                inputTaskTitle,
-                selectedInputTopic,
-                panelId
-              );
-              if (taskCreated) {
-                if (!everCreatedTaskTitles.includes(inputTaskTitle)) {
-                  setEverCreatedTaskTitles((everCreatedTaskTitles) => {
-                    const taskTitles = [
-                      ...everCreatedTaskTitles,
-                      inputTaskTitle,
-                    ];
-                    while (taskTitles.length > 5) taskTitles.shift();
-                    return taskTitles;
-                  });
-                }
-                setInputTaskTitle('');
-              }
-            }
-          }}
-        >
-          <Box display="flex" gap="10px">
-            <InputTopicSelector
-              selectedTopicId={selectedInputTopic ? selectedInputTopicId : ''}
-              setSelectedTopicId={setSelectedInputTopicId}
-              topicsList={topicsList}
-            />
-            <Autocomplete
-              sx={{ width: '100%' }}
-              id="inputTaskTitle"
-              disabled={!allowInput}
-              value={inputTaskTitle || ''}
-              inputValue={inputTaskTitle || ''}
-              onChange={(event, newValue) => {
-                setInputTaskTitle(newValue);
-              }}
-              onInputChange={(event, newValue) => {
-                const updatedValue =
-                  newValue.length === 1 ? newValue.toUpperCase() : newValue;
-                setInputTaskTitle(updatedValue);
-              }}
-              freeSolo
-              disableClearable
-              // options={everCreatedTaskTitles.map((option) => option)}
-              options={[].map((option) => option)}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  label="Add Task"
-                  size="small"
-                  //     InputProps={{
-                  //       ...params.InputProps,
-                  //       type: 'search',
-                  //     }}
-                  InputProps={{
-                    endAdornment: inputTaskTitle ? (
-                      <IconButton
-                        size="small"
-                        onClick={() => {
-                          setInputTaskTitle('');
-                        }}
-                      >
-                        <ClearIcon fontSize="10" />
-                      </IconButton>
-                    ) : null,
-                  }}
-                />
-              )}
-            />
-            <Button
-              sx={{
-                borderColor: 'rgb(196,196,196)',
-                width: '13.8%',
-                minWidth: '40px',
-              }}
-              size="small"
-              variant="outlined"
-              type="submit"
-            >
-              <ArrowForwardIcon color="action" fontSize="small" />
-            </Button>
-          </Box>
-        </form>
+        <Box display="flex">
+          <ViewSelector
+            setStacksCount={setStacksCount}
+            stacksCount={stacksCount}
+          />
+          <TopicsFilter
+            deleteTopic={deleteTopic}
+            selectedTopicFilterIndex={selectedTopicFilterIndex}
+            setSelectedTopicFilterIndex={setSelectedTopicFilterIndex}
+            topicsList={topicsList}
+          />
+        </Box>
         <Box
-          marginTop="10px"
           sx={{
-            display: 'flex',
-            flexFlow: 'row wrap',
-            justifyContent: 'space-between',
-            gap: '10px',
+            width: '22.9%',
           }}
         >
-          <Box display="flex">
-            <ViewSelector
-              setStacksCount={setStacksCount}
-              stacksCount={stacksCount}
-            />
-            <TopicsFilter
-              deleteTopic={deleteTopic}
-              selectedTopicFilterIndex={selectedTopicFilterIndex}
-              setSelectedTopicFilterIndex={setSelectedTopicFilterIndex}
-              topicsList={topicsList}
-            />
-          </Box>
-          <Box
-            sx={{
-              width: '22.9%',
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+              const topicName = event.target.topicName.value;
+              const creationStarted = addTopic(topicName);
+              if (creationStarted) {
+                setInputTopicName('');
+              }
             }}
           >
-            <form
-              onSubmit={(event) => {
-                event.preventDefault();
-                const topicName = event.target.topicName.value;
-                const creationStarted = addTopic(topicName);
-                if (creationStarted) {
-                  setInputTopicName('');
+            <TextField
+              name="topicName"
+              onChange={(event) => {
+                let value = event.target.value;
+                if (value.length === 1) {
+                  value = value.toUpperCase();
                 }
+                setInputTopicName(value);
               }}
-            >
-              <TextField
-                name="topicName"
-                onChange={(event) => {
-                  let value = event.target.value;
-                  if (value.length === 1) {
-                    value = value.toUpperCase();
-                  }
-                  setInputTopicName(value);
-                }}
-                label="Add Topic"
-                size="small"
-                value={inputTopicName}
-              />
-            </form>
-          </Box>
+              label="Add Topic"
+              size="small"
+              value={inputTopicName}
+            />
+          </form>
         </Box>
       </Box>
+
       {allTasksView ? (
         <Box paddingTop="10px">
           <AllTasksView
